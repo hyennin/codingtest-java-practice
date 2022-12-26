4955508b4a88c7ed418630fd141250afd7309082
hyennin <chm050202@gmail.com>
2022-12-26 09:44:20 +0900

    두 수 비교

--- compare.java
+++ compare.java
@@ -1,2 +1,17 @@
 public class compare {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(2, 5));
+        System.out.println(new Solution().solution(5, 5));
+    }
+}
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+
+        if (num1 == num2) answer = 1;
+        else answer = -1;
+
+        return answer;
+    }
 }
